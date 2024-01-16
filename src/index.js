26a5ad3d42fe671f0294558ddef30405e6504fd6
VaishnaviNeelaraddi <vaishnavineelaraddi05@gmail.com>
2024-01-17 00:00:07 +0530

    unit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />,document.getElementById("root")
+);
